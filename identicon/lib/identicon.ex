81693f20acec5d0081317f0362b42e8d0aa7dd2a
Leonardo Silva <leosilvadev@gmail.com>
2018-08-26 23:33:13 +0200

    adding doc dependency and doc some functions

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,6 +1,20 @@
 defmodule Identicon do
 
-  def main(input) do
+  @moduledoc """
+    Provides methods to build, draw and save Identicons
+  """
+
+  @doc """
+    Create a new Identicon based in the given `input`.
+    The file generated will be the values of `input` as png image.
+    returns if the image was created or not (:ok, :error)
+
+  ## Examples
+
+        iex> Identicon.create("leonardo")
+        :ok
+  """
+  def create(input) do
     input
     |> hash_input
     |> pick_color
@@ -9,10 +23,33 @@ defmodule Identicon do
     |> save_image(input)
   end
 
-  def save_image(image, input) do
-    File.write("#{input}.png", image)
+  @doc """
+    Save the given `image` as png with the given `filename`
+    returns if the image was saved or not (:ok, :error)
+
+  ## Examples
+
+        iex> image = :egd.create(250, 250)
+        iex> Identicon.save_image(:egd.render(image), "leonardo")
+        :ok
+  """
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
   end
 
+  @doc """
+    Draw the blocks of the given `image`
+    returns the binary of this image
+
+  ## Examples
+
+        iex> image = %Identicon.Image{blocks: [
+        ...> %Identicon.Block{rgb: {255, 255, 255}, from: {0, 0}, to: {50, 50}}
+        ...> ]}
+        iex> is_binary(Identicon.draw_image(image))
+        true
+
+  """
   def draw_image(%Identicon.Image{blocks: blocks}) do
     image = :egd.create(250, 250)
 
