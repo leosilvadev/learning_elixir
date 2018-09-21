b558732cdef2d720081795c76409bc9849fa195c
Leonardo Silva <leosilvadev@gmail.com>
2018-09-21 08:54:26 +0200

    set websocket connect

--- discuss/web/channels/comments_channel.ex
+++ discuss/web/channels/comments_channel.ex
@@ -1,12 +1,12 @@
-defmodule Dicuss.CommentsChannel do
+defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join() do
-
+  def join(name, _params, socket) do
+    {:ok, %{message: "Welcome"}, socket}
   end
 
   def handle_in() do
-    
+
   end
 
 end
