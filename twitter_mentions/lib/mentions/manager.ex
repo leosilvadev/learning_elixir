a47416d05a9ef4abe3717ede278308c77662025e
Leonardo Silva <leosilvadev@gmail.com>
2018-12-02 11:38:43 +0100

    mention domain and it's manager functions

---
+++ twitter_mentions/lib/mentions/manager.ex
@@ -0,0 +1,175 @@
+defmodule Mentions.Manager do
+  require Logger
+
+  import String, only: [to_integer: 1]
+  import Ecto.Query
+
+  @moduledoc """
+    Contains functions used to handle one or more `Mentions.Mention`.
+    A Mention is a tweet that mentions a specific username.
+  """
+
+  @months %{"Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4, "May" => 5, "Jun" => 6, "Jul" => 7, "Ago" => 8, "Sep" => 9, "Oct" => 10, "Nov" => 11, "Dec" => 12}
+
+  @doc """
+    Search for mentions to a given `mentioned` username or a given `metadata` (result from a previous search).
+    The result won't return more than a givne `number_mentions`.
+    Important: in Twitter, we cannot request more then 100 tweets at one call, so passing more than 100 as `number_mentions` will just return up to 100.
+
+  ### Examples
+
+      iex> Mentions.Manager.search("metallica", 1)
+      {[
+        %Mentions.Mention{
+          created_at: ~N[2018-12-02 07:50:42],
+          retweets: 1359,
+          search_for: "@metallica",
+          text: "@Metallica is just great!",
+          tweet_id: "1063136713090846147",
+          user: "leosilvadev"
+        }
+      ],
+      %{
+        completed_in: 0.09,
+        count: 1,
+        max_id: 1063136713090846149,
+        max_id_str: "1063136713090846149",
+        next_results: "?max_id=1063136713090846146&q=%40metallica&count=1&include_entities=1",
+        query: "%40metallica",
+        refresh_url: "?since_id=1063136713090846149&q=%40metallica&include_entities=1",
+        since_id: 0,
+        since_id_str: "0"
+      }}
+
+      iex> metadata = %{
+        completed_in: 0.09,
+        count: 1,
+        max_id: 1069136951289102335,
+        max_id_str: "1069136951289102335",
+        next_results: "?max_id=1069136763019440128&q=%40metallica&count=1&include_entities=1",
+        query: "%40metallica",
+        refresh_url: "?since_id=1069136951289102335&q=%40metallica&include_entities=1",
+        since_id: 0,
+        since_id_str: "0"
+      }
+      iex> Mentions.Manager.search("metallica", metadata)
+      {[
+        %Mentions.Mention{
+          created_at: ~N[2018-12-02 07:52:53],
+          retweets: 100,
+          search_for: "@metallica",
+          text: "@Metallica is just great 2!",
+          tweet_id: "1069136951289102334",
+          user: "leosilvadev"
+        }
+      ],
+      %{
+        completed_in: 0.09,
+        count: 1,
+        max_id: 1069136951289102339,
+        max_id_str: "1069136951289102339",
+        next_results: "?max_id=1069136951289102333&q=%40metallica&count=1&include_entities=1",
+        query: "%40metallica",
+        refresh_url: "?since_id=1069136951289102339&q=%40metallica&include_entities=1",
+        since_id: 0,
+        since_id_str: "0"
+      }}
+  """
+  @spec search(String.t, integer) :: {list(map()), String.t | map()}
+  def search("@" <> _ = mentioned, number_mentions), do:
+    next_mentions(mentioned, number_mentions, last_tweet_id(mentioned))
+
+  def search(%{} = metadata, _number_mentions), do:
+    next_mentions(metadata)
+
+  @doc """
+    Fetch the id of the last mention to a given `mentioned` username.
+
+  ### Examples
+
+      iex> Mentions.Manager.last_tweet_id("metallica")
+      "379283798237928312"
+
+      iex> Mentions.Manager.last_tweet_id("invalidusername")
+      nil
+  """
+  @spec last_tweet_id(String.t) :: String.t | nil
+  def last_tweet_id(mentioned) do
+    Mentions.Repo.one from m in "mentions",
+          where: m.search_for == ^mentioned,
+          order_by: [desc: m.created_at],
+          limit: 1,
+          select: m.tweet_id
+  end
+
+  @doc """
+    Persist the list of given `mentions` to the database in batch.
+
+  ### Examples
+
+      iex> mentions = [
+        %Mentions.Mention{
+          created_at: ~N[2018-12-02 07:52:53],
+          retweets: 100,
+          search_for: "@metallica",
+          text: "@Metallica is just great 2!",
+          tweet_id: "1069136951289102334",
+          user: "leosilvadev"
+        }
+      ]
+      iex> Mentions.Manager.persist(mentions)
+      1
+
+      iex> Mentions.Manager.persist([])
+      0
+  """
+  @spec persist(list(Mentions.Mention.t)) :: integer()
+  def persist(mentions) do
+    Logger.info "Persisting #{length(mentions)} mentions"
+    maps = mentions |> Enum.map(&(Map.from_struct(&1)))
+    {total_inserted, _} = Mentions.Repo.insert_all("mentions", maps)
+    Logger.info "#{total_inserted} mentions were persisted in the database"
+    total_inserted || 0
+  end
+
+  defp next_mentions("@" <> _ = mentioned, number_mentions, nil), do:
+    next_mentions(fn () -> ExTwitter.search(mentioned, count: number_mentions, search_metadata: true) end)
+
+  defp next_mentions("@" <> _ = mentioned, number_mentions, last_tweet) do
+    Logger.info "A mention about #{mentioned} was found (#{last_tweet}), fetching all the mentions after this one"
+    next_mentions(fn () -> ExTwitter.search(mentioned, count: number_mentions, since_id: last_tweet, search_metadata: true) end)
+  end
+
+  defp next_mentions(%{} = metadata), do:
+    next_mentions(fn () -> ExTwitter.search_next_page(metadata) end)
+
+  defp next_mentions(search) do
+    %{statuses: statuses, metadata: metadata} = search.()
+    %{query: search_for} = metadata
+
+    mentions = statuses
+    |> Stream.map(&(to_mention(URI.decode(search_for), &1)))
+    |> Enum.to_list
+
+    {mentions, metadata}
+  end
+
+  defp to_mention(search_for, %{id_str: id, text: text, created_at: created_at, retweet_count: retweet_count, user: %{screen_name: screen_name}}), do:
+    %Mentions.Mention{tweet_id: id, search_for: search_for, user: screen_name, text: text, created_at: to_datetime(created_at), retweets: retweet_count}
+
+  defp to_datetime(str) do
+    [_, month_str, day, time, _, year] = String.split(str)
+    [hour, minute, second] = String.split(time, ":")
+
+    {:ok, datetime} = NaiveDateTime.new(
+      to_integer(year),
+      @months |> Map.get(month_str),
+      to_integer(day),
+      to_integer(hour),
+      to_integer(minute),
+      to_integer(second)
+    )
+    datetime
+  end
+
+end
