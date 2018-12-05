fb799f328d4d41012cf9020f106e47d561a0ab96
Leonardo Silva <leosilvadev@gmail.com>
2018-12-05 22:02:30 +0100

    set cookies for all envs

--- twitter_mentions/rel/config.exs
+++ twitter_mentions/rel/config.exs
@@ -10,6 +10,7 @@ use Mix.Releases.Config,
 environment :dev do
   set dev_mode: true
   set include_erts: false
+  set cookie: :"Bj7k2={/o%;4&.who{8r|^8xM1*OG=J|,~NB8GH(=|5;>m:syw;q4A=CxmC8SnK*"
   set config_providers: [
     {Mix.Releases.Config.Providers.Elixir, ["${RELEASE_ROOT_DIR}/etc/config.exs"]}
   ]
@@ -18,6 +19,7 @@ end
 environment :test do
   set dev_mode: true
   set include_erts: false
+  set cookie: :"c>My{36?p)u&%C`lg{MUD^h3o,r;0R[E6>tJ]Xb|ip^K/Be$9eOENXBkKN$,&m$D"
   set config_providers: [
     {Mix.Releases.Config.Providers.Elixir, ["${RELEASE_ROOT_DIR}/etc/config.exs"]}
   ]
@@ -26,6 +28,7 @@ end
 environment :prod do
   set include_erts: true
   set include_src: false
+  set cookie: :"P=1}8:E^$St7]rGgrv@(ugK;FLr>u]Qu8C%g&%1h.Jkwf]3l2svBm|dib)~O>SFP"
   set vm_args: "rel/vm.args"
   set config_providers: [
     {Mix.Releases.Config.Providers.Elixir, ["${RELEASE_ROOT_DIR}/etc/config.exs"]}
