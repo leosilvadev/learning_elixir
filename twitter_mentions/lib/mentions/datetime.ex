a15bd2805e5b9d9c2fb2d4c798f82d97f21a4273
Leonardo Silva <leosilvadev@gmail.com>
2018-12-05 21:30:11 +0100

    datetime tests as doc

--- twitter_mentions/lib/mentions/datetime.ex
+++ twitter_mentions/lib/mentions/datetime.ex
@@ -1,11 +1,26 @@
 defmodule Mentions.Datetime do
 
+  @moduledoc """
+    Contains functions do parse Twitter datetime
+  """
+
   import String, only: [to_integer: 1]
 
   @months %{"Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4, "May" => 5, "Jun" => 6, "Jul" => 7, "Ago" => 8, "Sep" => 9, "Oct" => 10, "Nov" => 11, "Dec" => 12}
 
-  def parse(str) do
+  @doc """
+    Parses a given `str` to a `NaiveDateTime`
 
+  ### Examples
+
+      iex> Mentions.Datetime.parse("Sun Feb 25 18:11:01 +0000 2018")
+      ~N[2018-02-25 18:11:01]
+
+      iex> Mentions.Datetime.parse("10-09-2018 11:02:05")
+      nil
+  """
+  @spec parse(String.t) :: nil | NaiveDateTime.t()
+  def parse(str) do
     with [_, month_str, day, time, _, year] <- String.split(str),
          [hour, minute, second]             <- String.split(time, ":") do
 
--- twitter_mentions/test/mentions/datetime_test.exs
+++ twitter_mentions/test/mentions/datetime_test.exs
@@ -1,13 +1,4 @@
 defmodule Mentions.DatetimeTest do
   use ExUnit.Case
-
-  test "Should parse a valid datime" do
-    {:ok, expected} = NaiveDateTime.new(2018, 2, 25, 18, 11, 01)
-    datetime = Mentions.Datetime.parse("Sun Feb 25 18:11:01 +0000 2018")
-    assert expected == datetime
-  end
-
-  test "Should fail on invalid datetime format" do
-    assert is_nil(Mentions.Datetime.parse("10-09-2018 11:02:05")) == true
-  end
+  doctest Mentions.Datetime
 end
